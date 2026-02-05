5c3097adef55bca754403d5982d15a25115bfa50
Kautsar Hasby <kautsar20204@gmail.com>
2026-02-05 14:25:26 +0700

    feat(posts): added pagination

--- internal/handlers/posts/handler_posts.go
+++ internal/handlers/posts/handler_posts.go
@@ -8,29 +8,30 @@ import (
 	"github.com/kautsarhasby/situs-forum/internal/model/posts"
 )
 
-type postService interface{
-	CreatePost (ctx context.Context,userId int64, request posts.CreatePostRequest) error
-	CreateComment(ctx context.Context,postID, userID int64, request posts.CreateCommentRequest) error
-	UpsertUserActivity (ctx context.Context, postID, userID int64, request posts.UserActivityRequest) error
+type postService interface {
+	CreatePost(ctx context.Context, userId int64, request posts.CreatePostRequest) error
+	CreateComment(ctx context.Context, postID, userID int64, request posts.CreateCommentRequest) error
+	UpsertUserActivity(ctx context.Context, postID, userID int64, request posts.UserActivityRequest) error
+	GetAllPost(ctx context.Context, limit, offset int) (posts.GetAllPostResponse, error)
 }
-
 
 type Handler struct {
 	*gin.Engine
 	postSVC postService
 }
 
-func NewHandler(api *gin.Engine, postSvc postService) *Handler{
-	return  &Handler{
-		Engine: api,
+func NewHandler(api *gin.Engine, postSvc postService) *Handler {
+	return &Handler{
+		Engine:  api,
 		postSVC: postSvc,
 	}
 }
 
-func (h *Handler) RegisterRoute(){
+func (h *Handler) RegisterRoute() {
 	route := h.Group("posts")
 	route.Use(middleware.AuthMiddleware())
-	route.POST("/create",h.CreatePost)
-	route.POST("/comment/:postID",h.CreateComment)
+	route.POST("/create", h.CreatePost)
+	route.POST("/comment/:postID", h.CreateComment)
 	route.PUT("/user_activity/:postID", h.UpsertUserActivity)
+	route.GET("/", h.GetAllPost)
 }
--- internal/service/posts/service.go
+++ internal/service/posts/service.go
@@ -8,21 +8,22 @@ import (
 )
 
 type postsRepository interface {
-	CreatePost(ctx context.Context, model posts.PostModel) error 
+	CreatePost(ctx context.Context, model posts.PostModel) error
 	CreateComment(ctx context.Context, model posts.CommentModel) error
 	GetUserActivities(ctx context.Context, model posts.UserActivityModel) (*posts.UserActivityModel, error)
 	CreateUserActivites(ctx context.Context, model posts.UserActivityModel) error
 	UpdateUserActivites(ctx context.Context, model posts.UserActivityModel) error
+	GetAllPost(ctx context.Context, limit, offset int) (posts.GetAllPostResponse, error)
 }
 
 type service struct {
-	cfg *configs.Config
+	cfg      *configs.Config
 	postRepo postsRepository
 }
 
 func NewService(postRepo postsRepository, cfg *configs.Config) *service {
 	return &service{
-		cfg : cfg,
+		cfg:      cfg,
 		postRepo: postRepo,
 	}
 }
